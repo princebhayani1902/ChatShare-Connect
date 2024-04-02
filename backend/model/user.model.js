5bd6cfa6eefc9bc948b20667d36c43633ea517c9
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-02 16:26:54 +0530

    Connect to MongoDB

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -2,18 +2,14 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
-        username: {
-            type: String,
-            require: true,
-            unique: true
-        },
         fullName: {
             type: String,
             require: true
         },
         email: {
             type: String,
-            require: true
+            require: true,
+            unique: true
         },
         password: {
             type: String,
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
-import express from "express"
-
+import express from "express";
+import dotenv from "dotenv";
+import connectToMongoDB from "./db/ConnectedToMongoDB.js";
 const app = express();
+dotenv.config();
 
-const PORT = 5000;
-app.get("/",(req,res)=>{
-    res.send("Backend on");
-})
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`);
-})
+app.get("/", (req, res) => {
+  res.send("Backend on");
+});
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on ${PORT}`);
+});
