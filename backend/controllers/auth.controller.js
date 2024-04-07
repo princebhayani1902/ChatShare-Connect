bd431aca3d9a42ce2259a0649649a8db211ddb38
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-07 20:27:39 +0530

    logout route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -55,3 +55,14 @@ export const login = async (req, res, next) => {
         next(error);
     }
 }
+
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.clearCookie('jwt');
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
