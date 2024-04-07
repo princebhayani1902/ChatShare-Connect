12319c5d98a5bdc4010f8105ef21689eae319bb6
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-07 22:22:26 +0530

    send message endpoints and protect routes middleware

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectToMongoDB from "./db/ConnectedToMongoDB.js";
 import authRouter from "./routes/auth.routes.js";
+import messageRouter from "./routes/message.routes.js"
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRouter);
+app.use("/api/message",messageRouter);
 
 app.use((err,req,res,next)=>{
   const statusCode = err.statusCode || 500;
