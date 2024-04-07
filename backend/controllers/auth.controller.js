f70d5316765e2dc968b935d2bb455a7546dc59f9
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-07 20:18:40 +0530

    add token and cookie with signup and login(signin)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/errors.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res, next) => {
     const { fullName, email, password } = req.body;
@@ -11,15 +12,26 @@ export const signup = async (req, res, next) => {
     const hashPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ fullName, email, password: hashPassword });
     try {
-        await newUser.save();
-        res.status(201).json("User created successfully");
+        if (newUser) {
+            //generate JWT token here
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
     } catch (error) {
         res.status(500).json(error.message);
         // next(error);
     }
 }
 
-export const signin = async (req, res, next) => {
+export const login = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
@@ -30,7 +42,15 @@ export const signin = async (req, res, next) => {
         if (!validPassword) {
             return next(errorHandler(401, "Wrong credentials!"));
         }
-        res.status(201).json("User signin successfully");
+        generateTokenAndSetCookie(validUser._id, res);
+
+        res.status(200).json({
+            _id: validUser._id,
+            fullName: validUser.fullName,
+            email: validUser.email,
+            profilePic: validUser.profilePic
+        });
+
     } catch (error) {
         next(error);
     }
